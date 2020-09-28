faa3a8929e7fa808ab558f7364e8c31629574161
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-09-28 08:53:38 -0300

    first commit

---
+++ src/main/java/cl/crised/restsecurityspring/RestSecuritySpringApplication.java
@@ -0,0 +1,13 @@
+package cl.crised.restsecurityspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecuritySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSecuritySpringApplication.class, args);
+	}
+
+}
